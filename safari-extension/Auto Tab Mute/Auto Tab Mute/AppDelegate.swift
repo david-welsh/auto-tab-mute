e0ab39780d7195a6abdb64ec111ac6d41c1915f6
David Welsh <mail@davidwelsh.co.uk>
2023-11-04 02:46:51 +0000

    Remove safari conversion workflow and add xcode project:

---
+++ safari-extension/Auto Tab Mute/Auto Tab Mute/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Auto Tab Mute
+//
+//  Created by David Welsh on 04/11/2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
